68600f317a93ba0b627b0ad14802678db1e0ba0c
undefined <undefined>
2020-11-29 16:00:51 +0800

    提交

---
+++ template/src/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import App from './pages/hello.vue';
+
+new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App />'
+});
+
+// function test() {
+//   return 444444433;
+// }
+
+// console.log(test());
